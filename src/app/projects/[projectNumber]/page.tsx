e242611ce3be8b0d88f3d87609e0f9080803845f
Brandon Quon <bqa7@sfu.ca>
2025-06-02 20:57:05 -0700

    fix build error

--- src/app/projects/[projectNumber]/page.tsx
+++ src/app/projects/[projectNumber]/page.tsx
@@ -1,13 +1,14 @@
 import ProjectDetailsPage from "../../../../components/projects/ProjectDetailsPage";
 
 interface ProjectPageProps {
-  params: {
+  params: Promise<{
     projectNumber: string;
-  };
+  }>;
 }
 
-export default function ProjectPage({ params }: ProjectPageProps) {
-  const formattedProjectNumber = `(${params.projectNumber})`;
-  
+export default async function ProjectPage({ params }: ProjectPageProps) {
+  const { projectNumber } = await params;
+  const formattedProjectNumber = `(${projectNumber})`;
+
   return <ProjectDetailsPage projectNumber={formattedProjectNumber} />;
 }
