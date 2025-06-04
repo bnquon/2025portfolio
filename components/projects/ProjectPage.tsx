ccb03fac112e8735842ad1e8312ad4c2b0766570
Brandon Quon <bqa7@sfu.ca>
2025-06-03 21:02:26 -0700

    spacing

--- components/projects/ProjectPage.tsx
+++ components/projects/ProjectPage.tsx
@@ -3,7 +3,7 @@ import ProjectTable from "./ProjectTable";
 
 export default function ProjectPage() {
   return (
-    <PageContainer gap="sm:gap-28 gap-6">
+    <PageContainer gap="2xl:gap-28 sm:gap-12 gap-6">
       <section id="projects">
         <p className="2xl:text-4xl sm:text-3xl text-2xl">(Projects)</p>
       </section>
